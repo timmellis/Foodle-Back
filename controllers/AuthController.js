ea33b209bcb78f3b75406982565bc9c3f1e59f9d
timmellis <tim.m.ellis@gmail.com>
2022-04-15 12:19:42 -0400

    backend auth primary files (server.js, AuthRouter, AuthController, middleware/index.js)

---
+++ controllers/AuthController.js
@@ -0,0 +1,42 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+
+const Register = async (req, res) => {
+  try {
+    const {fullname, username, email, password } = req.body
+    let passwordDigest = await middleware.hashPassword
+    (password)
+    const user = await User.create({ username, email, paswordDigest, fullname })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.body.username },
+      raw: true
+    })
+    if (user && 
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {id: user.id, username: user.username }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token})
+    }
+    res.status(401).send( {status: 'Error', msg: 'Unauthorized, try again.'} )
+  } catch (error) {
+    throw error
+  }
+}
+
+
+module.exports = {
+  Register, 
+  Login
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+const AuthRouter = require('./routes/AuthRouter')
+// const PostRouter = require('./routes/PostRouter')
+
+const app = express()
+
+const port = process.env.PORT || 3001
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.use('/auth', AuthRouter)
+// app.use('/posts', PostRouter)
+
+app.listen(PORT, () => console.log(`Boom! Server running on port: ${PORT}`))
