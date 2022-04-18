a0c0908b4e0d2b95f94d9a826be626dae14fea1c
timmellis <tim.m.ellis@gmail.com>
2022-04-18 10:42:11 -0400

    added auth routers for session checks

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -4,4 +4,17 @@ const controller = require('../controllers/AuthController')
 router.post('/login', controller.Login)
 router.post('/register',controller.Register)
 
+router.post(
+  '/update',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePassword
+)
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
+
 module.exports = router
